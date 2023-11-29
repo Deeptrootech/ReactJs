62c940d7e9ef325c0a7e97045181bb37cd766fa1
Deep Pathak <deep.pathak@trootech.com>
2023-11-29 19:03:38 +0530

    [ADD] -- array crud

--- src/components/ArrayCRUD.js
+++ src/components/ArrayCRUD.js
@@ -2,22 +2,65 @@ import React, { useState } from "react";
 
 function ArrayCRUD() {
   const initialValue = "";
-  const [car, CarsReflect] = useState(initialValue);
+  const [fruit, FruitsReflect] = useState(initialValue); // state for reflecting entered value in input tag
+  const [fruitlist, setFruitsList] = useState(["fruit1", "fruit2"]); // state for add entered value in array
 
-
-  const CarsArray = () => {
-    console.log("CarsArray");
+  const AddFruitsInArray = (e) => {
+    e.preventDefault();
+    setFruitsList([...fruitlist, fruit]);
+    FruitsReflect(""); // Clear the input field after adding the fruit
   };
+  const UpdateFruit = (updateindex, updatevalue) => {
+    const allFruitItems = [...fruitlist]
+    allFruitItems[updateindex] = updatevalue
+    setFruitsList(allFruitItems);
+  };
+  const removeFruit = (removeindex) => {
+    const updatedItems = fruitlist.filter(
+      (item, item_index) => item_index !== removeindex
+    );
+    setFruitsList(updatedItems);
+  };
+
   return (
     <div>
-      <form onSubmit={CarsArray}>
-        <input type='text' value={car} onChange={e => {CarsReflect(e.target.value)}} />
-        <input type='button' value={"Submit"} />
+      <form onSubmit={AddFruitsInArray}>
+        <input
+          type='text'
+          value={fruit}
+          name='myfruit'
+          onChange={(e) => {
+            FruitsReflect(e.target.value);
+          }}
+        />
+        <input
+          type='submit'
+          style={{ background: "#00800087" }}
+          value={"Submit"}
+        />
       </form>
       <div>
-        <h1>Motors List:</h1>
+        <h1>Fruits List:</h1>
         <ol>
-          <li>TATA</li>
+          {fruitlist.map((item, index) => (
+            <li key={index}>
+              <input
+                type='text'
+                value={item}
+                name='updatefruit'
+                onChange={(e) => {
+                  UpdateFruit(index, e.target.value);
+                }}
+              />
+              
+              <input
+                style={{ background: "#ff000094" }}
+                type='button'
+                value='delete'
+                onClick={() => removeFruit(index)}
+              />
+            </li>
+          ))}
         </ol>
       </div>
     </div>
