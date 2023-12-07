3c2289743ce9b21cb29dfa5bf214176c928c0f2b
Deep Pathak <deep.pathak@trootech.com>
2023-12-07 16:04:53 +0530

    [ADD] -- new project of quotes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
-import Accordian from "./components/Accordian.js";
-const items = [
-  {
-    title: "Section 1",
-    content: "This is the content of section 1",
-  },
-  {
-    title: "Section 2",
-    content: "This is the content of section 2",
-  },
-  {
-    title: "Section 3",
-    content: "This is the content of section 3",
-  },
-];
+import Quote from "./components/Quote.js";
+
 function App() {
   return (
     <div className='App'>
-      <Accordian items={items} />
+      <Quote />
     </div>
   );
 }
