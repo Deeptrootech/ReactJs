1057f09e50c5490a95652e60aca0027aa620c925
Deep Pathak <deep.pathak@trootech.com>
2023-12-08 13:16:05 +0530

    [ADD] -- add git search project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import ShoppingList from "./components/ShoppingList";
+import GitSearch from "./components/GitSearch";
 
 function App() {
   return (
     <div className='App'>
-    <ShoppingList />
+    <GitSearch />
     </div>
   );
 }
