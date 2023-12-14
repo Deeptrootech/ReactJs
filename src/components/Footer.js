09100fa391fc4e312ee5ad2f3cd01f5f83eed1b0
Deep Pathak <deep.pathak@trootech.com>
2023-12-14 19:52:06 +0530

    WIP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,43 @@
 import { Link } from "react-router-dom";
-import "./Footer.css"
-
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <section>Footer Example 3</section>
+    <footer className='footer-distributed'>
+      <div className='footer-right'>
+        <Link to='#'>
+          <i classNameName='fa fa-facebook'></i>
+        </Link>
+        <Link to='#'>
+          <i classNameName='fa fa-twitter'></i>
+        </Link>
+        <Link to='#'>
+          <i classNameName='fa fa-linkedin'></i>
+        </Link>
+        <Link to='#'>
+          <i classNameName='fa fa-github'></i>
+        </Link>
+      </div>
 
-      <footer className='footer-distributed'>
-        <div className='footer-right'>
-          <Link to='#'>
-            <i classNameName='fa fa-facebook'></i>
+      <div className='footer-left'>
+        <p className='footer-links'>
+          <Link className='link-1' to='#'>
+            Home
           </Link>
-          <Link to='#'>
-            <i classNameName='fa fa-twitter'></i>
-          </Link>
-          <Link to='#'>
-            <i classNameName='fa fa-linkedin'></i>
-          </Link>
-          <Link to='#'>
-            <i classNameName='fa fa-github'></i>
-          </Link>
-        </div>
 
-        <div className='footer-left'>
-          <p className='footer-links'>
-            <Link className='link-1' to='#'>
-              Home
-            </Link>
+          <Link to='#'>Blog</Link>
 
-            <Link to='#'>Blog</Link>
+          <Link to='#'>Pricing</Link>
 
-            <Link to='#'>Pricing</Link>
+          <Link to='#'>About</Link>
 
-            <Link to='#'>About</Link>
+          <Link to='#'>Faq</Link>
 
-            <Link to='#'>Faq</Link>
+          <Link to='#'>Contact</Link>
+        </p>
 
-            <Link to='#'>Contact</Link>
-          </p>
-
-          <p>Company Name &copy; 2015</p>
-        </div>
-      </footer>
-    </div>
+        <p>Company Name &copy; 2015</p>
+      </div>
+    </footer>
   );
 }
